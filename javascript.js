762bfeb8483109f2b9704e48574860658c1f2127
Nicholas Finney <nickfinney@Nicholass-MacBook-Pro.local>
2019-03-29 16:25:18 -0700

    styled the page

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 // INITIATING VARIABLES
 const body = document.querySelector('body');
 let container = document.createElement('div');
+let title = document.createElement('h1');
 let fragment = document.createDocumentFragment(); // variable to hold all the divs and add them all at once versus 
 let squaresPerSide = 16;
 
@@ -8,9 +9,18 @@ let squaresPerSide = 16;
 // SETTING VARIABLE CSS PROPERTIES
 body.style.margin = '0';
 body.style.padding = '0';
+
+title.textContent = "Etch-A-Sketch";
+title.style.margin = '100px auto 50px auto';
+title.style.textAlign = 'center';
+title.style.width = '100vw';
+
 container.classList.toggle('container');
+container.style.border = '10px solid red';
 container.style.display = 'grid';
-container.style.height = '100vh';
+container.style.height = '600px';
+container.style.margin = '0 auto';
+container.style.width = '600px';
 container.style.gridTemplateColumns = 'repeat(' + squaresPerSide + ', 1fr)';
 container.style.gridTemplateRows = 'repeat(' + squaresPerSide + ', 1fr)';
 
@@ -21,6 +31,7 @@ function createDivs(squaresPerSide) {
         var newDiv = document.createElement('div');
         newDiv.id = 'square-'+i;
         newDiv.className = 'gridbox';
+        newDiv.style.border = '1px solid black';
         // newDiv.addEventListener('mouseenter', mousedOver);
         newDiv.addEventListener('mouseenter', e => e.target.classList.add('moused-over') );
         fragment.appendChild(newDiv);
@@ -40,4 +51,5 @@ createDivs(squaresPerSide);
 
 // ADD VARIABLES TO HTML
 container.appendChild(fragment);
+body.appendChild(title);
 body.appendChild(container);
